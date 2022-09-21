9b948188e2e409a6f5b4139af25b8b1363613827
TiffanyHunnicutt <tiffany.hunnicutt@mykenzie.snhu.edu>
2022-09-21 17:32:33 -0500

    added classes

--- Application/src/main/java/com/kenzie/appserver/controller/model/CaseResponse.java
+++ Application/src/main/java/com/kenzie/appserver/controller/model/CaseResponse.java
@@ -1,4 +1,43 @@
 package com.kenzie.appserver.controller.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CaseResponse {
-}
+
+        @JsonProperty("caseNumber")
+        private String caseNumber;
+
+        @JsonProperty("name")
+        private String name;
+
+        @JsonProperty("caseType")
+        private String caseType;
+
+
+        public String getCaseNumber() {
+            return caseNumber;
+        }
+
+        public void setCaseNumber(String caseNumber) {
+            this.caseNumber = caseNumber;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getCaseType() {
+            return caseType;
+        }
+
+        public void setCaseType(String caseType) {
+            this.caseType = caseType;
+        }
+    }
+
--- Application/src/main/java/com/kenzie/appserver/controller/model/EvidenceResponse.java
+++ Application/src/main/java/com/kenzie/appserver/controller/model/EvidenceResponse.java
@@ -1,4 +1,51 @@
 package com.kenzie.appserver.controller.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class EvidenceResponse {
+    @JsonProperty("caseNumber")
+    private String caseNumber;
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("caseType")
+    private String caseType;
+
+    public String getEvidence() {
+        return evidence;
+    }
+
+    public void setEvidence(String evidence) {
+        this.evidence = evidence;
+    }
+
+    @JsonProperty("evidence")
+    private String evidence;
+
+    public String getCaseNumber() {
+        return caseNumber;
+    }
+
+    public void setCaseNumber(String caseNumber) {
+        this.caseNumber = caseNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCaseType() {
+        return caseType;
+    }
+
+    public void setCaseType(String caseType) {
+        this.caseType = caseType;
+    }
 }
