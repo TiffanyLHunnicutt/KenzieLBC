2937b5aca0d08bc0a1f8f1d03ffdf37e43f103c6
MaCherieTeahl <macherie.teahl@mykenzie.snhu.edu>
2022-09-21 22:48:38 -0400

    girlswhocode

--- Application/src/main/java/com/kenzie/appserver/service/model/Case.java
+++ Application/src/main/java/com/kenzie/appserver/service/model/Case.java
@@ -1,4 +1,65 @@
 package com.kenzie.appserver.service.model;
 
+import java.util.List;
+
 public class Case {
+    private final String caseId;
+    private final String timeStamp;
+    private String title;
+    private String author;
+    private String description;
+    private String timeDate;
+    private String location;
+    private List<String> potentialSuspects;
+    private Boolean openCase;
+
+    public Case(String caseId, String location,String timeStamp, String title, String author, String description, String timeDate, List<String> potentialSuspects, Boolean openCase) {
+        this.caseId = caseId;
+        this.timeStamp = timeStamp;
+        this.title = title;
+        this.author = author;
+        this.description = description;
+        this.timeDate = timeDate;
+        this.potentialSuspects = potentialSuspects;
+        this.openCase = openCase;
+        this.location = location;
+    }
+
+    public String getCaseId() {
+        return caseId;
+    }
+
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getTimeDate() {
+        return timeDate;
+    }
+
+    public List<String> getPotentialSuspects() {
+        return potentialSuspects;
+    }
+
+    public Boolean getOpenCase() {
+        return openCase;
+    }
+
+    public String getLocation() {
+        return location;
+    }
 }
+
+
--- Application/src/main/java/com/kenzie/appserver/service/model/Evidence.java
+++ Application/src/main/java/com/kenzie/appserver/service/model/Evidence.java
@@ -1,4 +1,52 @@
 package com.kenzie.appserver.service.model;
 
 public class Evidence {
+    private String caseId;
+    private String evidenceId;
+    private String timeStamp;
+   private String location;
+   private String timeDate;
+    private String author;
+   private String description;
+
+    public Evidence(String caseId, String evidenceId, String timeStamp, String location, String timeDate, String author, String description) {
+        this.caseId = caseId;
+        this.evidenceId = evidenceId;
+        this.timeStamp = timeStamp;
+        this.location = location;
+        this.timeDate = timeDate;
+        this.author = author;
+        this.description = description;
+    }
+
+    public String getCaseId() {
+        return caseId;
+    }
+
+    public String getEvidenceId() {
+        return evidenceId;
+    }
+
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getTimeDate() {
+        return timeDate;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }
+
+
+
