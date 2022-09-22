124d3e3e593805cc3ee36a3ad68c911662ab64cb
TatyanaLavrenov <tatyana.lavrenov@mykenzie.snhu.edu>
2022-09-21 19:07:40 -0700

    Records, my part of spring 1

--- Application/src/main/java/com/kenzie/appserver/repositories/model/CaseRecord.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/model/CaseRecord.java
@@ -1,5 +1,70 @@
 package com.kenzie.appserver.repositories.model;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBRangeKey;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+import net.bytebuddy.matcher.StringSetMatcher;
+
 //tati is doing record this week for model
+@DynamoDBTable(tableName = "CaseRecord")
 public class CaseRecord {
+    public String caseId;
+    public String timeStamp;
+    public String title;
+    public String author;
+    public String description;
+    public String location;
+    public String TimeDate;
+    // elise please look at the following line, StringSet was redlining so I went with that
+    public StringSetMatcher potentialSuspects;
+    public Boolean openCase;
+
+    @DynamoDBRangeKey(attributeName = "caseId")
+    public String getCaseId() {
+        return caseId;
+    }
+
+    @DynamoDBAttribute(attributeName = "timeStamp")
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    @DynamoDBAttribute(attributeName = "title")
+    public String getTitle() {
+        return title;
+    }
+
+    @DynamoDBAttribute(attributeName = "author")
+    public String getAuthor() {
+        return author;
+    }
+
+    @DynamoDBAttribute(attributeName = "description")
+    public String getDescription() {
+        return description;
+    }
+
+    @DynamoDBAttribute(attributeName = "location")
+    public String getLocation() {
+        return location;
+    }
+
+    @DynamoDBAttribute(attributeName = "TimeDate")
+    public String getTimeDate() {
+        return TimeDate;
+    }
+
+    @DynamoDBAttribute(attributeName = "potentialSuspects")
+    public StringSetMatcher getPotentialSuspects() {
+        return potentialSuspects;
+    }
+
+    @DynamoDBAttribute(attributeName = "openCase")
+    public Boolean getOpenCase() {
+        return openCase;
+    }
+
+
+
+
 }
--- Application/src/main/java/com/kenzie/appserver/repositories/model/EvidenceRecord.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/model/EvidenceRecord.java
@@ -1,5 +1,54 @@
 package com.kenzie.appserver.repositories.model;
 
+import com.amazonaws.services.dynamodbv2.datamodeling.*;
+
 //tati is doing record this week for model
+@DynamoDBTable(tableName = "EvidenceRecord")
 public class EvidenceRecord {
+    public String caseId;
+    public String evidenceId;
+    public String timeStamp;
+    public String location;
+    public String TimeDate;
+    public String author;
+    public String description;
+
+    @DynamoDBHashKey(attributeName = "caseId")
+    public String getCaseId() {
+        return caseId;
+    }
+
+    @DynamoDBHashKey(attributeName = "evidenceId")
+    public String getEvidenceId() {
+        return evidenceId;
+    }
+
+    @DynamoDBAttribute(attributeName = "timeStamp")
+    public String getTimeStamp() {
+        return timeStamp;
+    }
+
+    @DynamoDBAttribute(attributeName = "location")
+    public String getLocation() {
+        return location;
+    }
+
+    @DynamoDBAttribute(attributeName = "TimeDate")
+    public String getTimeDate() {
+        return TimeDate;
+    }
+
+    @DynamoDBAttribute(attributeName = "author")
+    public String getAuthor() {
+        return author;
+    }
+
+    @DynamoDBAttribute(attributeName = "description")
+    public String getDescription() {
+        return description;
+    }
+
+
+
+
 }
