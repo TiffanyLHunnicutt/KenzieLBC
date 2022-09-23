9dd0a775bdae2662432500237f11d748a8e14c69
elise_swinehart <elise.swinehart@mykenzie.snhu.edu>
2022-09-22 21:35:25 -0600

    set up the repositories to extend crud

--- Application/src/main/java/com/kenzie/appserver/repositories/EvidenceRepository.java
+++ Application/src/main/java/com/kenzie/appserver/repositories/EvidenceRepository.java
@@ -1,4 +1,9 @@
 package com.kenzie.appserver.repositories;
 
-public interface EvidenceRepository {
+import com.kenzie.appserver.repositories.model.EvidenceRecord;
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+@EnableScan
+public interface EvidenceRepository extends CrudRepository<EvidenceRecord, String> {
 }
