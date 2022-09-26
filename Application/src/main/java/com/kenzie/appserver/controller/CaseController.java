644b84cff6cfb2c61e9f3803dfbc8cfa1c89b2be
elise_swinehart <elise.swinehart@mykenzie.snhu.edu>
2022-09-25 22:57:58 -0600

    making a skeleton for caseController, not finished yet

--- Application/src/main/java/com/kenzie/appserver/controller/CaseController.java
+++ Application/src/main/java/com/kenzie/appserver/controller/CaseController.java
@@ -1,8 +1,12 @@
 package com.kenzie.appserver.controller;
 
+import com.kenzie.appserver.controller.model.CaseCreateRequest;
+import com.kenzie.appserver.controller.model.CaseResponse;
 import com.kenzie.appserver.service.CaseService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cases")
@@ -12,5 +16,23 @@ public class CaseController {
 
     CaseController(CaseService caseService) {
         this.caseService = caseService;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<CaseResponse>> getAllOpenCases() {
+        //replace with actual code
+        return null;
+    }
+
+    @GetMapping("/{caseId}")
+    public ResponseEntity<CaseResponse> getCaseById(@PathVariable("caseId") String caseId) {
+        //replace with actual code
+        return null;
+    }
+
+    @PostMapping
+    public ResponseEntity<CaseResponse> createCase(@RequestBody CaseCreateRequest createRequest) {
+        //replace this with actual code
+        return null;
     }
 }
