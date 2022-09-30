d77ab15eeb107ac51ce2a6019817626b570423c3
TatyanaLavrenov <tatyana.lavrenov@mykenzie.snhu.edu>
2022-09-29 21:30:51 -0700

    s2.1

--- Application/src/main/java/com/kenzie/appserver/service/CaseService.java
+++ Application/src/main/java/com/kenzie/appserver/service/CaseService.java
@@ -1,11 +1,14 @@
 package com.kenzie.appserver.service;
 
 import com.kenzie.appserver.repositories.CaseRepository;
+import com.kenzie.appserver.repositories.model.CaseRecord;
+import com.kenzie.appserver.repositories.model.ExampleRecord;
 import com.kenzie.appserver.service.model.Case;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CaseService {
@@ -16,17 +19,61 @@ public class CaseService {
     }
 
     public List<Case> findAllOpenCases() {
-        return new ArrayList<>();
+        List<Case> case = new ArrayList<>();
+
+        Iterable<CaseRecord> caseRecordIterable = caseRepository.findAll();
+        for (CaseRecord record : caseRecordIterable) {
+            record.add(new Case(record.getCaseId(),
+                    record.getTimeStamp(),
+                    record.getTitle(),
+                    record.getAuthor(),
+                    record.getDescription(),
+                    record.getLocation(),
+                    record.getPotentialSuspects(),
+                    record.getOpenCase();
+        }
+
+        return case;
     }
 
     public Case findCaseByCaseId(String id) {
-        return null;
+        Optional<CaseRecord> optionalRecord = caseRepository.findById(id);
+
+        if (optionalRecord.isPresent()) {
+            CaseRecord record = optionalRecord.get();
+            return new CaseRecord(record.getCaseId(id));
+        } else {
+            return null;
+        }
     }
 
     public Case addNewCase(Case caseToAdd) {
-        return null;
+        CaseRecord caseRecord = new CaseRecord();
+        caseRecord.setCaseId(caseRecord.getCaseId());
+        caseRecord.setTimeStamp(caseRecord.getTimeStamp());
+        caseRecord.setTitle(caseRecord.getTitle());
+        caseRecord.setAuthor(caseRecord.getAuthor());
+        caseRecord.setDescription(caseRecord.getDescription());
+        caseRecord.setLocation(caseRecord.getLocation());
+        caseRecord.setTimeDate(caseRecord.getTimeDate());
+        caseRecord.setPotentialSuspects(caseRecord.getPotentialSuspects());
+        caseRecord.setOpenCase(caseRecord.getOpenCase());
+        caseRepository.save(caseRecord);
+        return caseToAdd;
     }
 
+
     public void updateCase(Case caseToUpdate) {
+        CaseRecord caseRecord = new CaseRecord();
+        caseRecord.setCaseId(caseRecord.getCaseId());
+        caseRecord.setTimeStamp(caseRecord.getTimeStamp());
+        caseRecord.setTitle(caseRecord.getTitle());
+        caseRecord.setAuthor(caseRecord.getAuthor());
+        caseRecord.setDescription(caseRecord.getDescription());
+        caseRecord.setLocation(caseRecord.getLocation());
+        caseRecord.setTimeDate(caseRecord.getTimeDate());
+        caseRecord.setPotentialSuspects(caseRecord.getPotentialSuspects());
+        caseRecord.setOpenCase(caseRecord.getOpenCase());
+        caseRepository.save(caseRecord);
     }
 }
