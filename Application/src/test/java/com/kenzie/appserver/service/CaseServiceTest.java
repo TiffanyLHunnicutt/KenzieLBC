548c8783295754d75cfa132cd219de4d859c3601
TatyanaLavrenov <tatyana.lavrenov@mykenzie.snhu.edu>
2022-09-29 21:04:14 -0700

    s2

---
+++ Application/src/test/java/com/kenzie/appserver/service/CaseServiceTest.java
@@ -0,0 +1,24 @@
+package com.kenzie.appserver.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CaseServiceTest {
+
+    @Test
+    void findAllOpenCases() {
+    }
+
+    @Test
+    void findCaseByCaseId() {
+    }
+
+    @Test
+    void addNewCase() {
+    }
+
+    @Test
+    void updateCase() {
+    }
+}
