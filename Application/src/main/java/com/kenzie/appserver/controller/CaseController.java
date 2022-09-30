b8a6cba8a77c3c63aab7e24cdf7e4039284c5468
TatyanaLavrenov <tatyana.lavrenov@mykenzie.snhu.edu>
2022-09-29 21:43:25 -0700

    s2.2

--- Application/src/main/java/com/kenzie/appserver/controller/CaseController.java
+++ Application/src/main/java/com/kenzie/appserver/controller/CaseController.java
@@ -3,7 +3,10 @@ package com.kenzie.appserver.controller;
 import com.kenzie.appserver.controller.model.CaseCreateRequest;
 import com.kenzie.appserver.controller.model.CaseResponse;
 import com.kenzie.appserver.controller.model.CaseUpdateRequest;
+import com.kenzie.appserver.controller.model.ExampleResponse;
 import com.kenzie.appserver.service.CaseService;
+import com.kenzie.appserver.service.model.Case;
+import com.kenzie.appserver.service.model.Example;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,7 +31,21 @@ public class CaseController {
     @GetMapping("/{caseId}")
     public ResponseEntity<CaseResponse> getCaseById(@PathVariable("caseId") String caseId) {
         //replace with actual code
-        return null;
+        Case case = caseService.findCaseByCaseId(caseId);
+        if (case == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        CaseResponse caseResponse = new CaseResponse();
+        caseResponse.setCaseId(caseResponse.getCaseId());
+        caseResponse.setTimeStamp(caseResponse.getTimeStamp());
+        caseResponse.setAuthor(caseResponse.getAuthor());
+        caseResponse.setDescription(caseResponse.getDescription());
+        caseResponse.setLocation(caseResponse.getLocation());
+        caseResponse.setTimeDate(caseResponse.getTimeDate());
+        caseResponse.setPotentialSuspects(caseResponse.getPotentialSuspects());
+        caseResponse.setTitle(caseResponse.getTitle());
+        return ResponseEntity.ok(caseResponse);
     }
 
     @PostMapping
