317d3065e0ab484a1adda63deef120669eaf54fb
elise_swinehart <elise.swinehart@mykenzie.snhu.edu>
2022-10-09 22:11:47 -0600

    trying to get front end to work

--- Frontend/src/api/CaseClient.js
+++ Frontend/src/api/CaseClient.js
@@ -5,37 +5,67 @@ export default class CaseClient extends BaseClass {
 
     constructor(props = {}){
         super();
-        const methodsToBind = ['clientLoaded', 'getAllOpenCases', 'getCase', 'createCase', 'updateCase'];
+        const methodsToBind = ['clientLoaded', 'getCase', 'getAllOpenCases', 'createCase', 'updateCase'];
         this.bindClassMethods(methodsToBind, this);
         this.props = props;
         this.clientLoaded(axios);
+    }
 
+    clientLoaded(client) {
+        this.client = client;
+        if (this.props.hasOwnProperty("onReady")){
+            this.props.onReady();
         }
-         clientLoaded(client) {
-             this.client = client;
-             if (this.props.hasOwnProperty("onReady")){
-                 this.props.onReady();
-             }
-         }
-             async getExample(id, errorCallback) {
-                 try {
-                     const response = await this.client.get(`/example/${id}`);
-                     return response.data;
-                 } catch (error) {
-                     this.handleError("getCase", error, errorCallback)
-                 }
-             }
-             async createExample(name, errorCallback) {
-                     try {
-                         const response = await this.client.post(`example`, {
-                             name: name
-                         });
-                         return response.data;
-                     } catch (error) {
-                         this.handleError("createCase", error, errorCallback);
-                     }
-                 }
-handleError(method, error, errorCallback) {
+    }
+
+    async getCase(caseId, errorCallback) {
+        try {
+            const response = await this.client.get(`/cases/${caseId}`);
+            return response.data;
+        } catch (error) {
+            this.handleError("getCase", error, errorCallback)
+        }
+    }
+
+    async getAllOpenCases(errorCallback) {
+        try {
+            const response = await this.client.get(`/cases/all`);
+            return response.data;
+        } catch(error) {
+            this.handleError("getAllOpenCases", error, errorCallback);
+        }
+    }
+
+    async createCase(title, author, description, location, timeDate, potentialSuspects, errorCallback) {
+        try {
+            const response = await this.client.post(`cases`, {
+                title: title,
+                author: author,
+                description: description,
+                location: location,
+                timeDate: timeDate,
+                potentialSuspects: potentialSuspects
+            });
+            return response.data;
+        } catch (error) {
+            this.handleError("createCase", error, errorCallback);
+        }
+    }
+
+    async updateCase(caseId, description, potentialSuspects, openCase, errorCallback) {
+        try {
+            const response = await this.client.put(`/cases/${caseId}`, {
+                description: description,
+                potentialSuspects: potentialSuspects,
+                openCase: openCase
+            });
+            return response.data;
+        } catch (error) {
+            this.handleError("updateCase", error, errorCallback);
+        }
+    }
+
+    handleError(method, error, errorCallback) {
         console.error(method + " failed - " + error);
         if (error.response.data.message !== undefined) {
             console.error(error.response.data.message);
